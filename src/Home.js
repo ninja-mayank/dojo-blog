c05091bcd9ffde088241deec0fed65f06d840c75
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2023-06-03 13:20:59 +0530

    functions as props

--- src/Home.js
+++ src/Home.js
@@ -9,12 +9,14 @@ const Home = () => {
         {title: 'Web dev top tips', body: 'lorem ipsum ...', author: 'Vishal', id: 3},
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return (  
         <div className="home">
-            <BlogList blogs = {blogs} title = "All blogs!" />
-            <BlogList blogs = {blogs.filter((blog) => 
-                blog.author === 'Mayank'
-            )} title = "Mayank's blogs!" />
+            <BlogList blogs = {blogs} title = "All blogs!" handleDelete = {handleDelete} />
         </div>
     );
 }
--- src/blogList.js
+++ src/blogList.js
@@ -1,7 +1,7 @@
 const BlogList = (props) => {
     const blogs = props.blogs;
     const title = props.title;
-    console.log(props,blogs);
+    const handleDelete = props.handleDelete;
     return (
         <div className="blogList">
             <h2>{title}</h2>
@@ -9,6 +9,7 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author} </p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
                 </div>
             ))}
         </div>
