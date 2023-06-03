ecd5b889832cc9286e7e43973aa6425f203099c0
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2023-06-03 13:34:10 +0530

    useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
         {title: 'Web dev top tips', body: 'lorem ipsum ...', author: 'Vishal', id: 3},
     ]);
 
+    const[name,setName] = useState('Mayank');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
@@ -16,11 +18,13 @@ const Home = () => {
 
     useEffect(() => {
         console.log('use effect ran');
-    })
+    },[name])
 
     return (  
         <div className="home">
             <BlogList blogs = {blogs} title = "All blogs!" handleDelete = {handleDelete} />
+            <button onClick={() => setName('lol')}>Change name</button>
+            <p>{name}</p>
         </div>
     );
 }
